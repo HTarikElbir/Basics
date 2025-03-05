83ebc2d13d4a3f89162fe918a3850ea94daa7859
HTarık <244035008@ogr.cbu.edu.tr>
2025-03-05 04:17:04 +0300

    Course controller added

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basics.Controllers
+{
+    public class CourseController : Controller
+    {
+        public string Index()
+        {
+            return "course/index";
+        }
+
+        public string List()
+        {
+            return "course/list";
+        }
+    }
+}
