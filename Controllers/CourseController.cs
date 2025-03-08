b8aca559c6552d624e505b9e00f4271caee0e19a
HTarık <244035008@ogr.cbu.edu.tr>
2025-03-09 00:29:56 +0300

    Details Page Configured

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -22,27 +22,17 @@ namespace Basics.Controllers
 
         public IActionResult List()
         {
-            // Create a list of courses
-            var courses = new List<Course>()
-            {
-                new Course { Id = 1, Title = "ASP.NET Core" , Description = "Great course", Image ="ASP.NET.webp" },
-                new Course { Id = 2, Title = "Javascript" , Description = "Great course", Image ="Js.webp" },
-                new Course { Id = 3, Title = "Python" , Description = "Great course", Image ="Python.webp" }
-            };
+          
             // Return the list of courses to the view
-            return View(courses);
+            return View(Repository.Courses);
         }
-        public IActionResult Details()
+        public IActionResult Details(int? id)
         {
-            // Create a new course instance
-            Course course = new Course();
-
-            // Set course properties
-            course.Id = 1;
-            course.Title = "ASP.NET Core MVC";
-            course.Description = "Great course";
-            course.Image = "ASP.NET.webp";
-
+            if (id == null)
+            {
+                return Redirect("/Course/list");
+            }
+            var course = Repository.GetById(id);
             // Return the course to the view
             return View(course);
         }
---
+++ Models/Repository.cs
@@ -0,0 +1,31 @@
+﻿using System.Dynamic;
+
+namespace Basics.Models
+{
+    public class Repository
+    {
+
+        private static readonly List<Course> _courses = new List<Course>()
+        {
+            // Create a new course instance
+            new Course { Id = 1, Title = "ASP.NET Core" , Description = "Great course", Image ="ASP.NET.webp" },
+            new Course { Id = 2, Title = "Javascript" , Description = "Great course", Image ="Js.webp" },
+            new Course { Id = 3, Title = "Python" , Description = "Great course", Image ="Python.webp" }
+        };
+
+
+        // Create a list of courses
+        public static List<Course> Courses
+        {
+            get
+            {
+                return _courses;
+            }
+        }
+
+        public static Course? GetById(int? id)
+        {
+            return _courses.FirstOrDefault(c => c.Id == id);
+        }
+    }
+}
